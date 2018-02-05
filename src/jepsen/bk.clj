fd909964d3a89dbb060e45c64ed70cd964a8be9b
Ivan Kelly <ivank@apache.org>
2018-02-05 17:17:27 +0000

    Use independent keys

--- src/jepsen/bk.clj
+++ src/jepsen/bk.clj
@@ -11,10 +11,12 @@
              [core :as jepsen]
              [db :as db]
              [generator :as gen]
+             [independent :as independent]
              [nemesis :as nemesis]
              [os :as os]
              [store :as store]
              [tests :as tests]]
+            [jepsen.checker.timeline :as timeline]
             [jepsen.util :refer [meh]]
             [jepsen.control.net :as net]
             [jepsen.control.util :as cu]
@@ -31,12 +33,12 @@
 
 (def register-service-port 3111)
 (def register-service-dir "/opt/register-service")
-(def register-service-log-file "/var/log/register-service.log")
+(def register-service-log-file "/var/log/register-service/register-service.log")
 (def register-service-systemd-name "register-service")
 
 (def bookkeeper-systemd-name "bookkeeper")
 (def bookkeeper-dir "/opt/bookkeeper")
-(def bookkeeper-log-file "/var/log/bookkeeper.log")
+(def bookkeeper-log-file "/var/log/bookkeeper/bookkeeper.log")
 
 (def zookeeper-data-dir "/var/lib/zookeeper")
 (def zookeeper-log-file "/var/log/zookeeper/zookeeper.log")
@@ -44,195 +46,8 @@
 (def debian-stretch
   (reify os/OS
     (setup! [_ test node]
-      (info node "setting up debian")
-      (binding [c/*sudo* nil]
-        (c/exec :apt-get :install :-y :--force-yes :sudo))
-      (debian/setup-hostfile!)
-      (debian/maybe-update!)
-      (debian/install [:wget
-                       :curl
-                       :vim
-                       :man-db
-                       :faketime
-                       :ntpdate
-                       :unzip
-                       :iptables
-                       :psmisc
-                       :tar
-                       :bzip2
-                       :openjdk-8-jdk
-                       :iputils-ping
-                       :iproute
-                       :rsyslog
-                       :logrotate])
-      (meh (net/heal)))
+       (meh (net/heal)))
     (teardown! [_ test node])))
-
-(defn- zoo-cfg-ids
-  "Get a deterministic mapping of zookeeper nodes to ids"
-  [zk-nodes]
-  (zipmap (sort zk-nodes) (map #(+ 1 %) (range))))
-
-(defn- zoo-cfg-servers
-  "Build list of servers for zoo.cfg"
-  [zk-nodes]
-  (->>
-   (map (fn [pair]
-          (let [host (first pair)
-                id (second pair)]
-            (str "server." id "=" host ":2888:3888")))
-        (zoo-cfg-ids zk-nodes))
-   (str/join "\n")))
-
-(defn- zoo-cfg
-  "Build zoo.cfg from a list of servers"
-  [zk-nodes]
-  (str
-   (slurp (clojure.java.io/resource "zoo.cfg"))
-   "\n"
-   (zoo-cfg-servers zk-nodes)))
-
-(defn- install-zk!
-  "Install zookeeper on node"
-  [nodes node]
-  (info node "installing zookeeper")
-  (debian/install [:zookeeperd :zookeeper :zookeeper-bin])
-  (c/exec :echo (zoo-cfg nodes) :> "/etc/zookeeper/conf/zoo.cfg")
-  (c/exec :mkdir :-p zookeeper-data-dir)
-  (c/exec :chown :-R "zookeeper:zookeeper" zookeeper-data-dir)
-  (c/exec :echo (get (zoo-cfg-ids nodes) node) :>
-          (str zookeeper-data-dir "/myid"))
-  (c/exec :service :zookeeper :restart))
-
-(defn- teardown-zk!
-  "Tear down zookeeper on a node"
-  [node]
-  (c/exec :service :zookeeper :stop)
-  (c/exec :rm :-rf zookeeper-data-dir)
-  (c/exec :rm :-f zookeeper-log-file))
-
-(defn- zk-connect-string
-  "Build a zookeeper connect string from a set of servers"
-  [zk-nodes]
-  (str/join "," zk-nodes))
-
-(defn- bookie-server-cfg
-  "Build bookie server config file contents"
-  [zk-nodes]
-  (str
-   (slurp (clojure.java.io/resource "bk_server.conf"))
-   "\nzkServers=" (zk-connect-string zk-nodes) "\n"))
-
-(defn- create-systemd-unit-file!
-  [node name directory & args]
-  (let [localfile (str "/tmp/" node "-" name ".service")
-        remotefile (str "/etc/systemd/system/" name ".service")]
-    (spit
-     localfile
-     (str "[Unit]
-Description=" name "
-After=network.target
-
-[Service]
-ExecStart=" (str/join " " args) "
-WorkingDirectory=" directory "
-KillMode=control-group
-RestartSec=1s
-Restart=on-failure
-Type=simple
-
-[Install]
-WantedBy=multi-user.target
-"))
-    (c/upload localfile remotefile)))
-
-(def jepsen-start-file "/tmp/jepsen-start")
-
-(defn- mark-jepsen-start! []
-  (c/exec :touch jepsen-start-file))
-
-(defn- since-start []
-  (c/exec :stat :-c :%y jepsen-start-file :| :cut :-c :1-19))
-
-(defn- dump-journal [unit file]
-  (c/exec :journalctl :-u unit :-S (since-start) :> file))
-
-(defn- download-or-upload [node uberjar]
-  (if (.exists (io/file uberjar))
-    (let [destfile (io/file register-service-dir "register-service.jar")
-          dest (str destfile)]
-      (info node "Local file " uberjar " uploading to " dest)
-      (c/upload uberjar dest))
-    (c/exec :curl :-L :-o "register-service.jar" uberjar)))
-
-(defn- install-register-service!
-  "Install register service on node"
-  [nodes node register-service-uberjar]
-  (info node "installing register service")
-  (c/exec :mkdir :-p register-service-dir)
-
-  (binding [c/*dir* register-service-dir]
-    (download-or-upload node register-service-uberjar)
-    (create-systemd-unit-file!
-     node register-service-systemd-name register-service-dir
-     "/usr/bin/java" "-jar" "register-service.jar"
-     "-z" (zk-connect-string nodes)
-     "-p" (str register-service-port)
-     "-t" (str 6000)) ; 6sec is minimum for default zk setup
-    (c/exec :systemctl :daemon-reload)
-    (c/exec :systemctl :start register-service-systemd-name)))
-
-(defn- teardown-register-service!
-  "Tear down register service on a node"
-  [node]
-  (binding [c/*dir* register-service-dir]
-    (c/exec :systemctl :stop register-service-systemd-name)
-    (dump-journal register-service-systemd-name
-                  register-service-log-file)))
-
-(defn- maybe-upload-bk [node bookkeeper-tarball]
-  (if (.exists (io/file bookkeeper-tarball))
-    (let [destfile (io/file cu/tmp-dir-base "bookkeeper.tgz")
-          dest (str destfile)
-          desturl (str (.toURL destfile))]
-      (info node "Local file " bookkeeper-tarball " uploading to " desturl)
-      (c/upload bookkeeper-tarball dest)
-      desturl)
-    bookkeeper-tarball))
-
-(defn- install-bk!
-  "Install bookkeeper on node"
-  [version nodes node bookkeeper-tarball]
-  (info node "installing bookkeeper " bookkeeper-tarball)
-  (cu/install-tarball!
-   node
-   (maybe-upload-bk node bookkeeper-tarball)
-   bookkeeper-dir)
-  (binding [c/*dir* bookkeeper-dir]
-    (c/exec :echo (bookie-server-cfg nodes) :> "conf/bk_server.conf")
-    (c/exec "bin/bookkeeper" :shell :bookieformat
-            :--deleteCookie :--force :--nonInteractive)
-    (if (= (first (sort nodes)) node)
-      (c/exec "bin/bookkeeper" :shell :metaformat
-              :--force :--nonInteractive))
-    (c/exec :mkdir :-p "logs")
-    (create-systemd-unit-file!
-     node bookkeeper-systemd-name
-     bookkeeper-dir (str bookkeeper-dir "/bin/bookkeeper") "bookie")
-    (c/exec :systemctl :daemon-reload)
-    (c/exec :systemctl :start bookkeeper-systemd-name)))
-
-(defn- teardown-bk!
-  "Tear down bookkeeper on a node"
-  [node]
-  (info node "tearing down bookkeeper")
-  (c/exec :systemctl :stop bookkeeper-systemd-name)
-  (try
-    (c/exec :pkill :-f :-9 "bookkeeper")
-    (catch RuntimeException e)) ; ignore
-  (c/exec :rm :-rf (str bookkeeper-dir "/bk-journal"))
-  (c/exec :rm :-rf (str bookkeeper-dir "/bk-data"))
-  (dump-journal bookkeeper-systemd-name bookkeeper-log-file))
 
 (defn db
   "Install bookkeeper and zookeeper on nodes"
@@ -240,27 +55,17 @@ WantedBy=multi-user.target
   (reify
     db/DB
     (setup! [a test node]
-      (info node "installing something" version)
-      (mark-jepsen-start!)
-      (let [nodes (:nodes test)]
-        (install-zk! nodes node)
-        (install-bk! version nodes node bookkeeper-tarball)
-        (install-register-service! nodes node register-service-uberjar))
-      (info node "all installed, waiting for up")
       (loop [seconds 30]
         (if (= seconds 0)
           (Exception. "Register service not up")
-          (let [url (resource-url node register-service-port)
+          (let [url (resource-url node register-service-port "key1")
                 result (rs/get-value url)]
             (if (f/failed? result)
               (do
                 (Thread/sleep 1000)
                 (recur (- seconds 1)))
               (info node "all up"))))))
-    (teardown! [b test node]
-      (teardown-register-service! node)
-      (teardown-bk! node)
-      (teardown-zk! node))
+    (teardown! [b test node])
     db/LogFiles
     (log-files [_ test node]
       [register-service-log-file
@@ -281,12 +86,14 @@ WantedBy=multi-user.target
 (defn client
   "A client to access the register service"
   [server]
-  (let [url (resource-url server register-service-port)]
-    (reify client/Client
-      (setup! [_ test node]
-        (client node))
+  (reify client/Client
+    (setup! [_ test node]
+      (client node))
 
-      (invoke! [this test op]
+    (invoke! [this test op]
+      (let [[k v] (:value op)
+            url (resource-url server register-service-port (str "key" k))]
+        (println "key " k)
         (case (:f op)
           :read (let [res (rs/get-value url)]
                   (if (f/failed? res)
@@ -294,20 +101,19 @@ WantedBy=multi-user.target
                       (assoc op :type :fail))
                     (let [seq-no (:seq res)]
                       (assoc op :type :ok
-                             :value (:value res)))))
-          :write (let [value (:value op)
-                       res (rs/set-value! url value)]
+                             :value (independent/tuple k (:value res))))))
+          :write (let [res (rs/set-value! url v)]
                    (assoc op :type (ok-fail-or-unknown? res)))
-          :cas   (let [[value value'] (:value op)
+          :cas   (let [[value value'] v
                        get-res (rs/get-value url)]
                    (if (or (f/failed? get-res)
                            (not= (:value get-res) value))
                      (assoc op :type :fail)
                      (let [seq-no (:seq get-res)
                            set-res (rs/set-value! url value' :seq-no seq-no)]
-                       (assoc op :type (ok-fail-or-unknown? set-res)))))))
+                       (assoc op :type (ok-fail-or-unknown? set-res))))))))
 
-      (teardown! [_ test]))))
+    (teardown! [_ test])))
 
 (defn bk-test
   [opts]
@@ -320,9 +126,17 @@ WantedBy=multi-user.target
           :client (client nil)
           :nemesis (nemesis/partition-random-halves)
           :model  (model/cas-register 0)
-          :checker checker/linearizable
-          :generator (->> (gen/mix [r w cas])
-                          (gen/stagger (/ 1 (:requests-per-second opts)))
+          :checker (checker/compose
+                    {:perf (checker/perf)
+                     :timeline (timeline/html)
+                     :linear (independent/checker checker/linearizable)})
+          :generator (->> (independent/concurrent-generator
+                           5
+                           (range)
+                           (fn [k]
+                             (->> (gen/mix [r w cas])
+                                  (gen/stagger (/ 1 (:requests-per-second opts)))
+                                  (gen/limit 100))))
                           (gen/nemesis
                            (gen/seq (cycle [(gen/sleep 5)
                                             {:type :info, :f :start}
